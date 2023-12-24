5c5dfae1929653e482f983baa354996e30b72ead
Ekrem <ekremkavas2012@gmail.com>
2023-12-24 21:35:57 +0300

    çalışmadevam

---
+++ javascript_çalışmam/introduction.js
@@ -0,0 +1,31 @@
+console.log("hello");
+console.log(typeof "zeynep");
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+/*
+let firstName = "ekrem";
+let firstName = "ekrem";
+let firstName = "ekrem";
+let firstName = "ekrem";
+
+*/
+
+let firstName = "Zeynep";
+let lastName = "Arslan";
+let country = "Turkey";
+let city = "Gaziantep";
+let age = 200;
+let isMarried = false;
+console.log(firstName, lastName, country, city, age, isMarried);
+
+let firstName = "Arslan";
+let lastName = "ufuki";
+let country = "Turkey";
+let city = "Trabzon";
+let age = 20;
+let isMarried = true;
+
+console.log(firstName, lastName, country, city, age, isMarried);
