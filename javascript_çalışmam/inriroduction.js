4d0fd63fe96f5a3074d3be9b171cb1df0c1a5573
Ekrem <ekremkavas2012@gmail.com>
2023-12-24 21:08:28 +0300

    24aralık23

---
+++ javascript_çalışmam/inriroduction.js
@@ -0,0 +1,12 @@
+console.log("hello");
+console.log(typeof "zeynep");
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+/*
+let firstName = "ekrem";
+let firstName = "ekrem";
+let firstName = "ekrem";
+let firstName = "ekrem";
